1fb5d43bfebdc2cdd26861d31d8b0a04327e62d1
Joon Jang <w.joon.jang@gmail.com>
2021-05-10 13:12:38 -0700

    - added model folder

---
+++ EnglishEducationTool/Chat.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EnglishEducationTool
+{
+    public class Chat
+    {
+        public string UserResponse { get; set; }
+        public string AIResponse { get; set; }
+    }
+}
